f5da9ddbde038fd3b02c82a3879ded4369905153
AgDecode <sahagusev2006@yandex.ru>
2024-04-12 14:39:46 +0400

    add load-balancer

---
+++ eureka-client-2/src/main/java/ru/micro/eurekaclient2/controller/HelloController.java
@@ -0,0 +1,15 @@
+package ru.micro.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String test(){
+        return "hello";
+    }
+}
--- eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package ru.micro.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test(){
-        return "test2";
+        return id;
     }
 }
