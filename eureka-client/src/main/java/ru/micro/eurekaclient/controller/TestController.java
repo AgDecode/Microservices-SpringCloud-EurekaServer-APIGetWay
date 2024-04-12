954fdece5456a21f302dbc9c79d9e715fdf2540f
AgDecode <sahagusev2006@yandex.ru>
2024-04-12 14:11:49 +0400

    edit spring.cloud.gateway.routes

--- eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
@@ -1,9 +1,12 @@
 package ru.micro.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
